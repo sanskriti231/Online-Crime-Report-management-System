fdbab0ede80eb5b42cf729f6e3e87cad9e027f70
Sumit Rathor <sumitrathor142272@gmail.com>
2024-11-16 12:18:15 +0530

    header , Footer

---
+++ footer.php
@@ -0,0 +1,16 @@
+<style>
+    .footer-p{
+        margin-bottom: 0px !important;
+    }
+</style>
+<footer class="pt-3 mt-4 text-white bg-black position-relative w-100 bottom-0">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="../" class="nav-link px-2 text-white">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-white">About Project</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-white">All Criminal</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Log Complaint</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Register</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Submit Feedback</a></li>
+    </ul>
+    <p class="footer-p text-center pb-4 text-white">© 2024 SSKH, Inc</p>
+</footer>
---
+++ header.php
@@ -0,0 +1,41 @@
+<style>
+        .logo-img {
+            width: 85px;
+            border-radius: 10px;
+        }
+
+        .roboto-bold {
+            font-family: "Roboto", sans-serif;
+            font-weight: 700;
+            font-style: normal;
+        }
+
+        .poppins-black {
+            font-family: "Poppins", sans-serif;
+            font-weight: 900;
+            font-style: normal;
+            font-size: 2rem;
+        }
+    </style>
+<header class="p-3 bg-black">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <a href="../" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                    <img src="../assets/img/logo.png" alt="Logo" class="logo-img">
+                </a>
+
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 roboto-bold">
+                    <li><a href="../" class="nav-link px-2 text-white">Home</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">About Project</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">All Criminal</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">Log Complaint</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">Register</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">Submit Feedback</a></li>
+                </ul>
+                <div class="text-end">
+                    <a href="../login" class="btn btn-outline-light me-2">Login</a>
+                    <a href="" class="btn btn-warning me-2">Sign-up</a>
+                </div>
+            </div>
+        </div>
+    </header>
